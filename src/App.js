c8376bd50500b5a706d85e5ec87a8f36dc76c735
Cruyun <1045671796@qq.com>
2018-07-24 21:48:20 +0800

    feat: header

--- src/App.js
+++ src/App.js
@@ -96,7 +96,6 @@ class Dropdown extends Component {
           onClick={() => {
             this.hide();
           }}
-          style={[styles.center]}
         >
           <Touchable>{children}</Touchable>
         </AnimatedView>
@@ -105,6 +104,7 @@ class Dropdown extends Component {
   }
 }
 var week = 1;
+
 class Header extends Component {
   constructor(props) {
     super(props);
@@ -113,37 +113,44 @@ class Header extends Component {
       currentWeek: week, // 当前周
       choosedWeek: week, // 设为当前周
       showsVerticalScrollIndicator: false,
-      chooseOnblur: false
+      isShow: false
     };
   }
 
   showWeekModal = () => {
-    this.refs.weekModal.show();
-    // this.setState({
-      
-    // })
+    if (!this.state.isShow) {
+      this.refs.weekModal.show();
+    }
+    else {
+      this.refs.weekModal.hide();
+    }
+    this.setState({
+      isShow: !this.state.isShow
+    })
   };
 
-  hideWeekModal = index => {
+  hideWeekModal = () => {
     this.setState({
-      currentWeek: index + 1
+      currentWeek: this.state.choosedWeek
     });
     this.refs.weekModal.hide();
   };
-
-  chooseWeek = index => {
-    // 
-  };
+  
+  choosingWeek = (index) => {
+    this.setState({
+      choosedWeek: index + 1
+    });
+  }
 
   render() {
     return (
       <View>
         <View style={[styles.header, styles.center]}>
-        <Touchable
+          <Touchable
             onPress={this.showWeekModal}
             style={[styles.choose_label, styles.center]}
           >
-            <Text>第{this.state.choosedWeek}周</Text>
+            <Text style={styles.choose_text}>第{this.state.choosedWeek}周</Text>
             <Image
               style={styles.down_triangle}
               source={require("./assets/triangle_down.png")}
@@ -151,44 +158,79 @@ class Header extends Component {
             />
           </Touchable>
         <View style={styles.dropdown_container}>
-        <Dropdown ref="weekModal">
-        <ScrollView 
-        ref={scrollView => {
-          this.scrollView = scrollView;
-        }}
-        style={styles.dropdown_list}
-        >
-            {this.WeekOptions.map((item, index) => {
-              return (
-                <View
-                  style={[styles.option_item, styles.center]}
-                  onClick={() => {
-                    this.chooseWeek(index);
-                  }}
-                >
-                  <Text style={styles.option_text}>
-                    第{this.WeekOptions[index]}周
-                  </Text>
-                </View>
-              )
-            })}
-        </ScrollView>
-        <Button onPress={() => this.changeWeek()} style={[styles.set_button]}>
-          <Text style={[styles.button_text]}>设为当前周</Text>
-        </Button>
-        </Dropdown>
-        </View>
+          <Dropdown ref="weekModal">
+            <Image
+              style={styles.up_triangle}
+              source={require("./assets/triangle_up.png")}
+              resizeMode="cover"
+            />
+            <View style={styles.list_container}>
+              <ScrollView 
+                ref={scrollView => {
+                  this.scrollView = scrollView;
+                }}
+                style={styles.dropdown_list}
+              >
+                {this.WeekOptions.map((item, index) => {
+                  return (
+                    <View style={styles.option_container}>
+                      <View
+                        style={this.state.choosedWeek == index + 1 ? [styles.choosing_option_item, styles.center, styles.option_item] : [styles.option_item, styles.center]}
+                        onClick={() => {
+                          this.choosingWeek(index);
+                        }}
+                      >
+                        <Text style={styles.option_text}>
+                        第{this.WeekOptions[index]}周
+                        </Text>
+                        {/* rax 中无 display: none */}
+                          {/* <Text style={this.state.currentWeek == index + 1 ? styles.current : styles.not_display}>当前</Text> */}
+                      </View>
+                    </View>
+                  )
+                })}
+              </ScrollView>
+              <Button onPress={() => this.hideWeekModal()} style={[styles.set_button, styles.center]}>
+                <Text style={[styles.button_text]}>设为当前周</Text>
+              </Button>
+            </View>
+            </Dropdown>
+          </View>
         </View>
       </View>
     );
   }
 }
 
+class Table extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={[styles.table]}>
+        <ScrollView
+          ref={(scrollView) => {
+            this.horizontalScrollView = scrollView;
+          }}
+          style={{
+            height: 1500,
+            backgroundColor: 'red'
+          }}
+          horizontal={true}
+        >
+          
+        </ScrollView>
+      </View>
+    )
+  }
+}
 class App extends Component {
   render() {
     return (
       <View style={styles.app}>
         <Header></Header>
+        <Table></Table>
       </View>
     );
   }
