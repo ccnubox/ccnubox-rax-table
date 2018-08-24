0c8af8d5e3cc6c558aff9d971c0d875996a7face
zxc0328 <120797887@qq.com>
2018-08-25 07:32:58 +0800

    reafactor: getCourse -> initCourseData && getCourseFromServer -> getCourseFromServerIMpl

--- src/services/table.js
+++ src/services/table.js
@@ -1,17 +1,20 @@
 import request from "../../box-ui/util/request.js";
 
 const TableService = {
-  getTableList() {
+  getTableList(options) {
+    let headers = {
+      'Accept': 'application/json',
+      'Sid': options.sid,
+      'Authorization': `Basic ${options.token}`
+    }
+    if (options.cookie) {
+      headers.Jsessionid = options.cookie.Jsessionid;
+      headers.Bigipserverpool = options.cookie.Bigipserverpool;
+    }
     return request({
       method: "GET",
       url: "https://ccnubox.muxixyz.com/api/table/",
-      headers: {
-        'Accept': 'application/json',
-        'Bigipserverpool': "89172160.20480.0000",
-        'Sid': "2016210773",
-        'Jsessionid': "0353EA4BBA48F76FDD41C36A52F10898",
-        'Authorization': "Basic MjAxNjIxMDc3MzowMzA2MTAxNDkwY3J5"
-      }
+      headers,
     });
   },
   addLesson(big, sid, jid, password, data) {
