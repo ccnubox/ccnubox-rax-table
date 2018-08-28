e481d07ebe0a571f897f3290ff8ddfe319305230
zxc0328 <120797887@qq.com>
2018-08-28 17:52:19 +0800

    feat: v2.0.0-beta1

--- webpack.config.update.js
+++ webpack.config.update.js
@@ -1,12 +1,23 @@
 // webpack.config.update.js
 const path = require('path');
+const webpack = require("webpack");
+const package = require("./package.json");
 
 module.exports = function update(webpackConfig) {
-  webpackConfig.entry["second.bundle"] = [
-    path.resolve(__dirname, "./node_modules/rax-scripts/lib/dev-utils/webpackHotDevClient.js"),
-    path.resolve(__dirname, "./node_modules/rax-hot-loader/patch.js"),
+  webpackConfig.entry["com.muxistudio.table.main"] = [
+    path.resolve(__dirname, "./src/index.js")
+  ];
+  webpackConfig.entry["com.muxistudio.tabble.add"] = [
     path.resolve(__dirname, "./src/second.js")
   ];
-  console.log("here", webpackConfig);
+  console.log("current env", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
+    webpackConfig.plugins[7].options.include = /\.js$/;
+    webpackConfig.plugins.push(
+      new webpack.BannerPlugin({
+        banner: `com.muxistudio.table, version ${package.version}, built time: ${Date()}`
+      })
+    );
+  }
   return webpackConfig;
 };
